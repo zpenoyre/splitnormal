82196a1115f07c2edc6643a1f6902d0978ea0731
Zephyr Penoyre <zephyrpenoyre@gmail.com>
2019-01-16 16:41:07 +0000

    can randomly sample, use the cdf and it's inverse

--- splitnormal.py
+++ splitnormal.py
@@ -1,4 +1,7 @@
 import numpy as np
+import scipy.special
+
+stdErf=scipy.special.erf(1/np.sqrt(2))
 
 def splitNormal(x,mu,sigma,cigma):
     epsilon=cigma/sigma
@@ -6,10 +9,31 @@ def splitNormal(x,mu,sigma,cigma):
     alphas[x>mu]=cigma
     return (1/np.sqrt(2*np.pi*sigma**2))*(2/(1+epsilon))*np.exp(-0.5*((x-mu)/alphas)**2)
 
+def cdf(x,mu,sigma,cigma):
+    epsilon=cigma/sigma
+    alphas=sigma*np.ones_like(x)
+    alphas[x>mu]=cigma
+    epsFactor=np.ones_like(x)
+    epsFactor[x>mu]=epsilon
+    return (1/(1+epsilon))*(1 + epsFactor*scipy.special((x-mu) / sqrt(2 * alphas**2)) )
+    
+def inverse(F,mu,sigma,cigma):
+    epsilon=cigma/sigma
+    alphas=sigma*np.ones_like(F)
+    alphas[F>1/(1+epsilon)]=cigma
+    betas=np.ones_like(F)
+    betas[F>1/(1+epsilon)]=1/epsilon
+    return mu + np.sqrt(2 * alphas**2) * scipy.special.erfinv(betas*((1+epsilon)*F -1))
+    
+def random(n,mu,sigma,cigma):
+    Fs=np.random.random(n)
+    xs=inverse(Fs,mu,sigma,cigma)
+    return xs
+
 def fit(xs):
     xs=np.sort(xs)
     N=xs.size
-    Delta=int(N*0.6826894921370859) #hardcoded version of erf(1/sqrt(2))
+    Delta=int(N*stdErf) #hardcoded version of erf(1/sqrt(2))
     
     js=np.arange(0,N-Delta-1)
     w_js=xs[js+Delta]-xs[js]
